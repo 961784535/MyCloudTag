7be938ca4fe9e282d694df1afbf59b0853c8c3b3
shenjun <961784535@qq.com>
2018-11-08 16:05:10 +0800

    Initial commit

---
+++ app/src/main/java/com/sj/mycloudtag/SearchDataPojo.java
@@ -0,0 +1,22 @@
+package com.sj.mycloudtag;
+
+/**
+ * 类描述：
+ * 作者：created by sj on 20182018/11/8 11:49
+ * 邮箱：
+ * 修改说明：
+ */
+public class SearchDataPojo {
+    private String content = "";
+
+    public String getContent() {
+        return content;
+    }
+
+    public SearchDataPojo setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+
+}
